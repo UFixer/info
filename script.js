367a43969ac08e3bf1cec7a5a4a04eefa32ce51e
You Fixer <157241692+UFixer@users.noreply.github.com>
2026-06-05 15:55:11 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function toggleMenu() {
   document.querySelector(".nav-links").classList.toggle("active");
+  document.querySelector(".hamburger").classList.toggle("active");
 }
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    document.querySelector(".nav-links").classList.remove("active");
+    document.querySelector(".hamburger").classList.remove("active");
+  });
+});
